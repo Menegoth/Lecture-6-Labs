3108dea0bebc3b6d0c93321780f516fcbde8f331
Antonio <antonioareas@me.com>
2020-10-07 01:11:39 -0700

    Add project files.

---
+++ Lecture6Lab1/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/// Week 6		Lab 1
+/// File Name: MainWindow.xaml.cs
+/// @author: Antonio Monteiro
+/// Date:  October 5th 2020
+
+namespace Lecture6Lab1 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e) {
+
+            //catch errors
+            try {
+                //assign values from window
+                int operation = operationList.SelectedIndex;
+                decimal leftHand = Convert.ToDecimal(leftTextBox.Text);
+                decimal rightHand = Convert.ToDecimal(rightTextBox.Text);
+                decimal result = 0;
+
+                //perform operations based on operator
+                switch(operation) {
+
+                    case 0: //case +
+                        result = leftHand + rightHand;
+                        resultsLabel.Content = result;
+                        break;
+
+                    case 1: //case -
+                        result = leftHand - rightHand;
+                        resultsLabel.Content = result;
+                        break;
+
+                    case 2: //case *
+                        result = leftHand * rightHand;
+                        resultsLabel.Content = result;
+                        break;
+
+                    case 3: //case /
+                        result = leftHand / rightHand;
+                        resultsLabel.Content = result;
+                        break;
+
+                    case 4: //case %
+                        result = leftHand % rightHand;
+                        resultsLabel.Content = result;
+                        break;
+
+                    default: //no operator selected
+                        result = 0;
+                        MessageBox.Show("No operation selected.");
+                        break;
+
+                }
+
+            }
+            catch (DivideByZeroException error) {
+                MessageBox.Show("ERROR: " + error.Message);
+            }
+            catch (FormatException error) {
+                MessageBox.Show("ERROR: " + error.Message);
+            }
+            catch (OverflowException error) {
+                MessageBox.Show("ERROR: " + error.Message);
+            }
+
+        }
+    }
+}
---
+++ Lecture6Lab2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+/// Week 6		Lab 2
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  October 5th 2020
+
+namespace Lecture6Lab2 {
+
+    //fraction class
+    class Fraction {
+
+        //instance variables
+        private int nominator;
+        private int denominator;
+
+        //constructor methods
+        public Fraction() {
+
+        }
+        public Fraction(int nominator, int denominator) {
+
+            this.nominator = nominator;
+            this.denominator = denominator;
+
+        }
+
+        //get and set nominator
+
+        public int GetNominator() {
+            return nominator;
+        }
+        public void SetNominator(int nominator) {
+            this.nominator = nominator;
+        }
+
+        //get and set denominator
+        public int GetDenominator() {
+            return denominator;
+        }
+
+        public void SetDenominator(int denominator) {
+            this.denominator = denominator;
+        } 
+
+        //division method
+        public double Divide() {
+            return (nominator + 0.0) / denominator;
+        }
+
+        //lowest common denominator method
+        public String LowestCommonDenominator() {
+            int lowest = 0;
+
+            //loop through every number at nominator value and below checking for divisible by 0
+            for (int i = nominator; i > 1; i--) {
+
+                if (nominator % i == 0 && denominator % i == 0) {
+                    lowest = i;
+                    break;
+                }
+
+            }
+
+            if (lowest == 0) {
+                return "The fraction " + nominator + "/" + denominator + " is already simplified.";
+            } else {
+                return nominator + "/" + denominator + " = " + nominator / lowest + "/" + denominator / lowest;
+            }
+            
+        }
+
+        //ToString and Equals
+        public override String ToString() {
+            return nominator + "/" + denominator;
+        }
+
+        public bool Equals(Fraction other) {
+            return nominator == other.nominator && denominator == other.denominator;
+        }
+
+    }
+
+    class Program {
+        static void Main(string[] args) {
+
+            //test fractions
+            Fraction testFraction = new Fraction(75, 150);
+            Fraction testFraction2 = new Fraction();
+
+            //testing set functions
+            testFraction2.SetNominator(1);
+            testFraction2.SetDenominator(2);
+
+            //testing get functions
+            Console.WriteLine(testFraction.GetNominator());
+            Console.WriteLine(testFraction.GetDenominator());
+
+            //testing tostring and equals functions
+            Console.WriteLine(testFraction.ToString());
+            Console.WriteLine(testFraction.Equals(testFraction2));
+
+            //testing math functions
+            Console.WriteLine(testFraction.Divide());
+            Console.WriteLine(testFraction.LowestCommonDenominator());
+
+        }
+    }
+}
